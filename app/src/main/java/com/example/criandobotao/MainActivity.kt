180f01bf26c97ba2c81012603a5a743005b2d604
Pedro-Corbari <pedrocorbari.dev@gmail.com>
2022-06-04 17:01:16 -0300

    First Commit, V1.

--- app/src/main/java/com/example/criandobotao/MainActivity.kt
+++ app/src/main/java/com/example/criandobotao/MainActivity.kt
@@ -4,34 +4,87 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
-import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var botao1: Button
     lateinit var botaores: Button
-    lateinit var botaoMin: Button
+    lateinit var plus: Button
+    lateinit var minus: Button
+    lateinit var mult: Button
+    lateinit var div: Button
     lateinit var num1: TextView
+    lateinit var ent1: TextView
+    lateinit var ent2: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        botao1 = findViewById(R.id.Botao)
         botaores = findViewById(R.id.buttonRes)
-        botaoMin = findViewById(R.id.buttonMin)
         num1 = findViewById(R.id.Numero)
+        ent1 = findViewById(R.id.ent1)
+        ent2 = findViewById(R.id.ent2)
 
+        plus = findViewById(R.id.plus)
+        minus = findViewById(R.id.minus)
+        div = findViewById(R.id.div)
+        mult = findViewById(R.id.mult)
+
+        var Entrada1: Int = 0
+        var Entrada2: Int = 0
         var Numeracao: Int = 0
 
-        botao1.setOnClickListener (object : View.OnClickListener {
+        plus.setOnClickListener (object : View.OnClickListener {
             override fun onClick(v: View?) {
-                val i = Numeracao + 1
-                Numeracao = i
 
+                Entrada1 = ent1.text.toString().toInt()
+                Entrada2 = ent2.text.toString().toInt()
+
+                val i = Entrada1 + Entrada2
+                Numeracao = i
+                num1.text = Numeracao.toString()
+
+            }
+
+        } )
+
+        minus.setOnClickListener (object : View.OnClickListener {
+            override fun onClick(v: View?) {
+
+                Entrada1 = ent1.text.toString().toInt()
+                Entrada2 = ent2.text.toString().toInt()
+
+                val i = Entrada1 - Entrada2
+                Numeracao = i
+                num1.text = Numeracao.toString()
+
+            }
+
+        } )
+
+        mult.setOnClickListener (object : View.OnClickListener {
+            override fun onClick(v: View?) {
+
+                Entrada1 = ent1.text.toString().toInt()
+                Entrada2 = ent2.text.toString().toInt()
+
+                val i = Entrada1 * Entrada2
+                Numeracao = i
+                num1.text = Numeracao.toString()
+
+            }
+
+        } )
+
+        div.setOnClickListener (object : View.OnClickListener {
+            override fun onClick(v: View?) {
+
+                Entrada1 = ent1.text.toString().toInt()
+                Entrada2 = ent2.text.toString().toInt()
+
+                val i = Entrada1 / Entrada2
+                Numeracao = i
                 num1.text = Numeracao.toString()
 
             }
@@ -40,20 +93,15 @@ class MainActivity : AppCompatActivity() {
 
         botaores.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
+                Entrada1 = 0
+                Entrada2 = 0
                 Numeracao = 0
                 num1.text = Numeracao.toString()
+                ent1.text = Entrada1.toString()
+                ent2.text = Entrada2.toString()
             }
         })
 
-        botaoMin.setOnClickListener (object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val i = Numeracao - 1
-                Numeracao = i
 
-                num1.text = Numeracao.toString()
-
-            }
-
-        } )
     }
 }
